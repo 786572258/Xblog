d0c100e012b97f866ae87b0676cbc7dae24e6237
maimai <595171801@qq.com>
2016-10-30 07:32:46 +0000

    小调整

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="row">
                 <div class="col-sm-12">
                     <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }}</a></span> |
-                    <span>蜀ICP备16008148号-2</span> |
+                    <span>备案号</span> |
                     <span><a href="{{env('GITHUB_FOOTER')}}"><i class="fa fa-github fa-fw"></i></a> </span>
                 </div>
             </div>
